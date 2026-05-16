4c048a019ad92e491383c399054f3009e58d98e9
Lucas Villarinho <lucasvillarinho1@gmail.com>
2026-05-16 16:23:44 -0300

    fix: correct zoxide command and update FZF options in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ starship init fish | source
 
 fzf --fish | source
 
-set -g zoxide_cmd cf
+set -g zoxide_cmd cd
 set -gx EZA_STANDARD_OPTIONS --group-directories-first --no-permissions --no-user --no-git --icons
 
 export FZF_DEFAULT_OPTS='--height 40%'
+
+source "$HOME/.cargo/env.fish"
